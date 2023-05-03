6f9733db788aa0a45844d9a50936b6eb8cf48e8f
David McConnell <dvmcconnell@gmail.com>
2023-05-03 12:33:08 -0600

    doc build and pylint warning (#25)
    * undo README change from external fork
    
    * ignore unused import since it's fine here
    
    * undo README change for DMC-test to fix later for another test
    
    * try permissions change

--- conftest.py
+++ conftest.py
@@ -1,7 +1,7 @@
 """Used to redefine hyalus fixtures to avoid the issues that arise from using pytest to test a pytest plugin"""
 
 # Import fixtures here so that pytest becomes aware of them for any tests
-from hyalus.run.python import fixture_run_dir
+from hyalus.run.python import fixture_run_dir  # pylint: disable=unused-import
 
 # Set up logging for the entire test suite here just once, within the main interpreter session
 from hyalus.utils.logging_utils import configure_logging
